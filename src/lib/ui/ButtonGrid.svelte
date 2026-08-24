e5365d881e0074ae5c41368e0d5da759dbb84dfe
flojoho <hofkas.flo@gmx.at>
2026-08-24 20:42:45 +0200

    make button grid resizeable

--- src/lib/ui/ButtonGrid.svelte
+++ src/lib/ui/ButtonGrid.svelte
@@ -10,37 +10,53 @@
   }
   let { diameter, spacing }: Props = $props();
 
-  $effect(() => {
-    if (buttonGridContainer) {
-      const divWidth = buttonGridContainer.offsetWidth;
-      const divHeight = buttonGridContainer.offsetHeight;
+  const updateGrid = () => {
+    if(!buttonGridContainer) return;
 
-      const tileSize = diameter + spacing;
+    const divWidth = buttonGridContainer.offsetWidth;
+    const divHeight = buttonGridContainer.offsetHeight;
 
-      const buttonsPerRow = Math.floor(divWidth / tileSize);
-      const buttonsPerColumn = Math.floor(divHeight / tileSize);
-      
-      const marginX = (divWidth - (buttonsPerRow * tileSize)) / 2;
-      const marginY = (divHeight - (buttonsPerColumn * tileSize)) / 2;
+    const tileSize = diameter + spacing;
 
-      const gridElements: NoteButtonDto[] = [];
+    const buttonsPerRow = Math.floor(divWidth / tileSize);
+    const buttonsPerColumn = Math.floor(divHeight / tileSize);
+    
+    const marginX = (divWidth - (buttonsPerRow * tileSize)) / 2;
+    const marginY = (divHeight - (buttonsPerColumn * tileSize)) / 2;
 
-      for (let countY = 0; countY < buttonsPerColumn; countY++) {
-        for(let countX = 0; countX < buttonsPerRow; countX++) {
-          const x = marginX + countX * tileSize;
-          const y = marginY + countY * tileSize;
+    const gridElements: NoteButtonDto[] = [];
 
-          const noteNumber = countX - (Math.floor(buttonsPerRow / 2)) - 5 * (countY - Math.floor(buttonsPerColumn / 2));
-          const noteButtonDto = new NoteButtonDto(noteNumber, x, y);
+    for (let countY = 0; countY < buttonsPerColumn; countY++) {
+      for(let countX = 0; countX < buttonsPerRow; countX++) {
+        const x = marginX + countX * tileSize;
+        const y = marginY + countY * tileSize;
 
-          gridElements.push(noteButtonDto);
+        const noteNumber = countX - (Math.floor(buttonsPerRow / 2)) - 5 * (countY - Math.floor(buttonsPerColumn / 2));
+        const noteButtonDto = new NoteButtonDto(noteNumber, x, y);
 
-          addToHighlightGroup(noteButtonDto);
-        }
+        gridElements.push(noteButtonDto);
+
+        addToHighlightGroup(noteButtonDto);
       }
-
-      set(gridElements);
     }
+
+    set(gridElements);
+  };
+
+  $effect(() => {
+    if(!buttonGridContainer) return;
+
+    updateGrid();
+
+    const observer = new ResizeObserver(() => {
+      updateGrid();
+    });
+
+    observer.observe(buttonGridContainer);
+
+    return () => {
+      observer.disconnect();
+    };
   });
 </script>
 
